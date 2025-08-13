4114bababffc0842272aababc870343b754b5457
Abdullah AYAZ <abdullah.ayaz@matriksdata.com>
2025-08-13 16:58:32 +0300

    Dilin dinamik olarak ayarlanabilmesi için URL parametreleri eklendi; ayrıca HTML belgesinin dil özniteliği güncellendi. Sitemap dosyasında son güncelleme tarihi 2025 olarak değiştirildi ve önbellek ayarları güncellendi.

--- json-formatter/sw.js
+++ json-formatter/sw.js
@@ -1,10 +1,21 @@
 const CACHE_NAME = 'json-formatter-v1';
 const urlsToCache = [
-  '/',
-  '/css/main.css',
-  '/js/json-formatter.js',
+  '/', 
+  '/?lang=tr',
+  '/?lang=en', 
+  '/icon.ico',
+  '/icon.png',
+  '/icon.svg',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png',
+  '/apple-touch-icon.png',
   '/android-chrome-192x192.png',
-  '/android-chrome-512x512.png'
+  '/android-chrome-512x512.png',
+  '/maskable-icon-512x512.png',
+  '/site.webmanifest',
+  '/sitemap.xml',
+  '/screenshots/desktop-1280x720.png',
+  '/screenshots/mobile-750x1334.png', 
 ];
 
 self.addEventListener('install', function(event) {
